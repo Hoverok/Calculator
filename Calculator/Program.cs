0d4e170af3390396fcc0c6ff0b3c8d4e28cfd420
Pavel <pavelchi91@gmail.com>
2019-04-22 12:43:20 +0300

    Divide, initial commit of develop branc

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,7 +9,7 @@ namespace Calculator
     {
         static void Main()
         {
-            double rez = 0, var1; // rez keeps track of the result, var1 used for operations
+            double rez = 10, var1; // rez keeps track of the result, var1 used for operations
             string CalcOperator; // reads the operator as a string
            
 
@@ -24,21 +24,48 @@ namespace Calculator
                 case "ADD":
                     rez = Program.Add(rez, var1);
                     break;
-                case "SUB":
-                    rez = Program.Sub(rez, var1);
+                case "SUBTRACT":
+                    rez = Program.Subtract(rez, var1);
+                    break;
+                case "DIVIDE":
+                    rez = Program.Divide(rez, var1);
                     break;
             }
             Console.WriteLine(rez);
         }
-     
+
+        /*
+ •	Add <number> - adds a number to the result
+•	Subtract <number> - subtracts number from the result
+•	Divide <number> - divides result by the number
+•	Times <number> - multiples number by the result 
+•	Power <number> - rises the result by power of number
+•	Root <number> - finds the base number of the result raised in power by number
+•	Clear – Makes result a 0
+•	Negative – Changes result to negative
+•	Positive – Changes result to positive
+•	Help – Shows all possible commands and descriptions
+
+         */
 
         public static double Add(double a, double b)
         {
             return a + b;
         }
 
-        public static double Sub (double a, double b) {
+        public static double Subtract(double a, double b) {
             return a - b;
             }
+        public static double Divide(double a, double b)
+        {
+            // need to make sure b!=0
+            if (b != 0) return a / b;
+            else
+            {
+                Console.WriteLine("Cannot divide by 0!");
+                return a;
+            }
+        }
+
     }
 }
